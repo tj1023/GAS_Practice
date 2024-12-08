09161ac903590fec4e00a4dd6f47ab44884c1869
tj1023 <htjeong1023@naver.com>
2024-12-09 00:34:16 +0900

    초기 커밋.

---
+++ Source/GAS_Practice/MyCharacterAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "MyCharacterAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_PRACTICE_API UMyCharacterAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
